e754a409db7c4e653ea014eb21e0180cebc9bc27
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2022-04-19 17:18:21 -0700

    Added documenation to FileSys.h

--- src/FileSys.h
+++ src/FileSys.h
@@ -1,7 +1,9 @@
 //
-// Created by Aaron Ishibashi on 4/15/22.
+// FileSys.h
+// SDG_Engine
+// 
+// Class that contains static helper functions to manipulate file paths.
 //
-
 #pragma once
 #include <string>
 using std::string;
@@ -10,10 +12,37 @@ namespace SDG
 {
     class FileSys {
     public:
+
+
+        /// <summary>
+        /// Cross-platform way to get the executable's root directory
+        /// </summary>
+        /// <returns></returns>
         static string GetBasePath();
+
+
+        /// <summary>
+        /// Helper function that returns the file name of a given path.
+        /// </summary>
+        /// <param name="path">The path to perform this function with</param>
+        /// <returns>The filename (including extension)</returns>
         static string GetFileName(const string &path);
+
+
+        /// <summary>
+        /// Gets the extension of a file from a given path.
+        /// </summary>
+        /// <param name="path">The path to the file</param>
+        /// <returns>The extension, not including the '.', or a blank string if
+        /// there is none.</returns>
         static string GetExtension(const string &path);
+
+
+        /// <summary>
+        /// Makes a path appended to the executable's base path.
+        /// </summary>
+        /// <param name="path">The path to append to the base path.</param>
+        /// <returns>The full appended path</returns>
         static string MakePath(const string &path);
     };
 }
-

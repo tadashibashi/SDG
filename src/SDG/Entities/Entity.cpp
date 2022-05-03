4d35902723ecd6695bc37d25fc530f19e3b4658c
Aaron Ishibashi <16326096+tadashibashi@users.noreply.github.com>
2022-05-03 09:34:58 -0700

    Started Entity class

---
+++ src/SDG/Entities/Entity.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Aaron Ishibashi on 5/1/22.
+//
+
+#include "Entity.h"
+
+namespace SDG
+{
+    Entity::Entity()
+    {
+
+    }
+
+    Entity::~Entity()
+    {
+
+    }
+}
---
+++ src/SDG/Entities/Entity.h
@@ -0,0 +1,18 @@
+//
+// Created by Aaron Ishibashi on 5/1/22.
+//
+
+#pragma once
+
+namespace SDG
+{
+    class Entity
+    {
+    public:
+        Entity();
+        ~Entity();
+
+    private:
+
+    };
+}

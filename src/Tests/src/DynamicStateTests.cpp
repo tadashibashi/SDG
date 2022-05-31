c2995daf378a780289623165c27b2c2dfffc6b2a
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2022-05-31 02:59:17 -0700

    Added DynamicState tests

---
+++ src/Tests/src/DynamicStateTests.cpp
@@ -0,0 +1,86 @@
+#include "SDG_Tests.h"
+#include <SDG/Logic/DynamicState.h>
+
+static void DummyFunc(float time) { }
+
+TEST_CASE("DynamicState tests", "[DynamicState]")
+{
+    DynamicState state;
+
+    SECTION("Has* functions on default state")
+    {
+        REQUIRE(!state.HasStart());
+        REQUIRE(!state.HasEnd());
+        REQUIRE(!state.HasUpdate());
+        REQUIRE(!state.HasPause());
+        REQUIRE(!state.HasResume());
+        REQUIRE(state.Empty());
+    }
+
+    SECTION("Set callback with lambda")
+    {
+        SECTION("Set OnStart")
+        {
+            state.OnStart([](float t) {});
+            REQUIRE(state.HasStart());
+        }
+
+        SECTION("Set OnUpdate")
+        {
+            state.OnUpdate([](float t) {});
+            REQUIRE(state.HasUpdate());
+        }
+
+        SECTION("Set OnEnd")
+        {
+            state.OnEnd([](float t) {});
+            REQUIRE(state.HasEnd());
+        }
+
+        SECTION("Set OnPause")
+        {
+            state.OnPause([](float t) {});
+            REQUIRE(state.HasPause());
+        }
+
+        SECTION("Set OnResume")
+        {
+            state.OnResume([](float t) {});
+
+            REQUIRE(state.HasResume());
+        }
+    }
+
+    SECTION("Set callback with global function")
+    {
+        SECTION("Set OnStart")
+        {
+            state.OnStart(DummyFunc);
+            REQUIRE(state.HasStart());
+        }
+
+        SECTION("Set OnUpdate")
+        {
+            state.OnUpdate(DummyFunc);
+            REQUIRE(state.HasUpdate());
+        }
+
+        SECTION("Set OnEnd")
+        {
+            state.OnEnd(DummyFunc);
+            REQUIRE(state.HasEnd());
+        }
+
+        SECTION("Set OnPause")
+        {
+            state.OnPause(DummyFunc);
+            REQUIRE(state.HasPause());
+        }
+
+        SECTION("Set OnResume")
+        {
+            state.OnResume(DummyFunc);
+            REQUIRE(state.HasResume());
+        }
+    }
+}
